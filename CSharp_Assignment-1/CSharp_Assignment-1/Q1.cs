d83b5897ee62c0e1afb13adf9142872ccca532ac
santuparsi <santuparsi@gmail.com>
2021-10-20 11:58:20 +0530

    new_commit

---
+++ CSharp_Assignment-1/CSharp_Assignment-1/Q1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Assignment_1
+{
+    class Q1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Q1 code write here.");
+        }
+    }
+}
---
+++ CSharp_Assignment-1/CSharp_Assignment-1/Q2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Assignment_1
+{
+    class Q2
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
